9d0098ce0488020db78aab542ebeaf86c714386c
genaro gonzalez <genarrogg@gmail.com>
2024-02-05 21:40:47 -0400

    configuracion del proyecto

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import "@css/style.scss";
+
+const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache(),
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
