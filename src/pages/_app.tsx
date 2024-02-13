25fc0034adfeed45c26bca17299770643bdfa5e0
genaro gonzalez <genarrogg@gmail.com>
2024-02-13 17:33:11 -0400

    eliminacion de apollo server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,8 @@
 import type { AppProps } from "next/app";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-import 'react-toastify/dist/ReactToastify.min.css';
-
-
+import "react-toastify/dist/ReactToastify.min.css";
 import "@css/style.scss";
 
-const client = new ApolloClient({
-  uri: '/api/graphql',
-  cache: new InMemoryCache(),
-});
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
