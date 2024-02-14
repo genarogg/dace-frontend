3ce2a8c3aa65a70c8c7608468fee068dd908c217
genaro gonzalez <genarrogg@gmail.com>
2024-02-13 21:24:20 -0400

    test

--- src/components/views/dashboard/mainContent/MainContent.tsx
+++ src/components/views/dashboard/mainContent/MainContent.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 
-
 interface MainContentProps {
   context: string;
   setContext: React.Dispatch<React.SetStateAction<string>>;
@@ -11,17 +10,10 @@ const MainContent: React.FC<MainContentProps> = ({ context, setContext }) => {
   const renderComponent = () => {
     switch (context) {
       case "initial":
-        return <p>holla</p>;
     }
   };
 
-  return (
-    <SwitchTransition>
-      <CSSTransition key={context} timeout={500} classNames="fade">
-        {renderComponent()}
-      </CSSTransition>
-    </SwitchTransition>
-  );
+  return <p>holla</p>;
 };
 
 export default MainContent;
