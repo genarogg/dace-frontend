8e7171d86ed42dda2ae6e12791446de149341fb8
genaro gonzalez <genarrogg@gmail.com>
2024-06-19 21:56:33 -0400

    d

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
-const BACKEND_URL = "http://localhost:8000";
-/* const BACKEND_URL = "https://dace-backend2.onrender.com"; */
+/* const BACKEND_URL = "http://localhost:8000"; */
+const BACKEND_URL = "https://dace-backend2.onrender.com";
 const RECAPTCHA_KEY = "6Lc3AWopAAAAAESKRKvIau3MEgONUYVi5C3K_BTh";
 const JWTSECRETO = "jwt-secret";
 
